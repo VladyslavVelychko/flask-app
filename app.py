80dceccd1afcbf6dc89faecc4b7a61b436baedd6
Vladyslav Velychko <velyk95@gmail.com>
2017-03-04 00:40:42 +0200

    Add new useful features

--- app.py
+++ app.py
@@ -2,11 +2,38 @@ from flask import Flask
 
 app=Flask(__name__)
 
-@app.route("/")
-@app.route("/hello")
+app.config["DEBUG"]=True
 
-def hello():
-	return "Hello, world!"
+@app.route("/")
+@app.route("/test/<search_query>")
+
+def search(search_query):
+	return search_query
+
+@app.route("/integer/<int:value>")
+
+def int_type(value):
+	print value+1
+	return "correct"
+
+@app.route("/float/<float:value>")
+
+def float_type(value):
+	print value+1
+	return "correct"
+
+@app.route("/path/<path:value>")
+
+def path_type(value):
+	print value
+	return "correct"
+
+@app.route("/name/<name>")
+def name(name):
+	if name.lower()=="vlad":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not found", 404
 
 if __name__=="__main__":
 	app.run()
